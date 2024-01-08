87a31e324379b7fd84f8a9dd4c37528ac40ac9ff
varun <shakunavetisaisriramvarun@gmail.com>
2024-01-08 11:28:09 +0530

    second commit

--- 10.5.2-15/codes/x.py
+++ 10.5.2-15/codes/x.py
@@ -3,8 +3,14 @@ import matplotlib.pyplot as plt
 
 # Define the function
 def my_function(n):
-    if n > 0:
-        return 61 + 2 * n
+    if n >= 0:
+        return 63 + 2 * n
+    else:
+        return 0
+
+def my_function2(n):
+    if n >= 0:
+        return 3 + 7 * n
     else:
         return 0
 
@@ -13,12 +19,16 @@ x_values = np.arange(-10, 11, 1)
 
 # Generate y values using the function
 y_values = [my_function(n) for n in x_values]
-# Plot the graph
-plt.plot(x_values, y_values, marker='o', linestyle='-', color='r')
-plt.xlabel('n')
-plt.ylabel('61f(n-1) + 2nf(n-1)')
-plt.grid(True)
-plt.figtext(0.5, 0.01, 'Figure: Plot of x(n) = 61f(n-1) + 2nf(n-1)', ha='center', va='center')
-plt.show()
+z_values = [my_function2(n) for n in x_values]
 
+# Stem plot for the first function (red)
+plt.stem(x_values, y_values, linefmt='r-', markerfmt='ro', basefmt='r-', label='x(n)')
+
+# Stem plot for the second function (blue)
+plt.stem(x_values, z_values, linefmt='b-', markerfmt='bo', basefmt='b-', label='y(n)')
+
+plt.xlabel('n')
+plt.legend()
+plt.grid(True)
+plt.show()
 
