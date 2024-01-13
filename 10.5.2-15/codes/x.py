25bdb8142f3d2bd1b5c5661b372c757659af572e
varun <shakunavetisaisriramvarun@gmail.com>
2024-01-13 22:42:44 +0530

    second commit

--- 10.5.2-15/codes/x.py
+++ 10.5.2-15/codes/x.py
@@ -22,10 +22,10 @@ y_values = [my_function(n) for n in x_values]
 z_values = [my_function2(n) for n in x_values]
 
 # Stem plot for the first function (red)
-plt.stem(x_values, y_values, linefmt='r-', markerfmt='ro', basefmt='r-', label='x(n)')
+plt.stem(x_values, y_values, linefmt='r-', markerfmt='ro', basefmt='r-', label='x_1(n)')
 
 # Stem plot for the second function (blue)
-plt.stem(x_values, z_values, linefmt='b-', markerfmt='bo', basefmt='b-', label='y(n)')
+plt.stem(x_values, z_values, linefmt='b-', markerfmt='bo', basefmt='b-', label='x_2(n)')
 
 plt.xlabel('n')
 plt.legend()
