f78ae1ec2dc528200d531b2df088fb588cfed91e
varun <shakunavetisaisriramvarun@gmail.com>
2024-01-20 19:35:10 +0530

    second commit

--- 10.5.2-15/codes/x.py
+++ 10.5.2-15/codes/x.py
@@ -1,27 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Define the function
-def my_function(n):
-    if n >= 0:
-        return 63 + 2 * n
-    else:
-        return 0
-
-def my_function2(n):
-    if n >= 0:
-        return 3 + 7 * n
-    else:
-        return 0
+def ap(a, d, start, end):
+    n_values = np.arange(start, end + 1)
+    return np.where(n_values >= 0, a + d * n_values, 0)
 
 # Generate x values using linspace
 x_values = np.linspace(-14, 14, 29)  # 29 points from -14 to 14
 
-# Generate y values using the function
-y_values = [my_function(n) for n in x_values]
-z_values = [my_function2(n) for n in x_values]
+y_values = ap(63, 2, x_values[0], x_values[-1])
 
-# Generate a list of colors for each stem
+z_values = ap(3, 7, x_values[0], x_values[-1])
+
 colors = ['r' if n != 12 else 'g' for n in x_values]
 
 # Stem plot for the first function
@@ -31,7 +21,7 @@ plt.stem(x_values, y_values, linefmt='-', markerfmt='ro', basefmt='-', label='x_
 plt.stem(x_values, z_values, linefmt='-', markerfmt='bo', basefmt='-', label='x_2(n)')
 
 # Highlight the point at n=12 with a different color
-plt.stem(12, my_function(12), linefmt='-', markerfmt='go', basefmt='-', label='n=12')
+plt.stem(12, ap(63, 2, 12, 12)[0], linefmt='-', markerfmt='go', basefmt='-', label='n=12')
 
 plt.xlabel('n')
 plt.legend()
