93aa1aa55de4279eec349acbe57ddec49cd9fee3
varun <shakunavetisaisriramvarun@gmail.com>
2024-01-20 17:43:44 +0530

    second commit

--- 10.5.2-15/codes/x.py
+++ 10.5.2-15/codes/x.py
@@ -14,18 +14,24 @@ def my_function2(n):
     else:
         return 0
 
-# Generate x values (integers)
-x_values = np.arange(-14, 15, 1)
+# Generate x values using linspace
+x_values = np.linspace(-14, 14, 29)  # 29 points from -14 to 14
 
 # Generate y values using the function
 y_values = [my_function(n) for n in x_values]
 z_values = [my_function2(n) for n in x_values]
 
-# Stem plot for the first function (red)
-plt.stem(x_values, y_values, linefmt='r-', markerfmt='ro', basefmt='r-', label='x_1(n)')
+# Generate a list of colors for each stem
+colors = ['r' if n != 12 else 'g' for n in x_values]
 
-# Stem plot for the second function (blue)
-plt.stem(x_values, z_values, linefmt='b-', markerfmt='bo', basefmt='b-', label='x_2(n)')
+# Stem plot for the first function
+plt.stem(x_values, y_values, linefmt='-', markerfmt='ro', basefmt='-', label='x_1(n)')
+
+# Stem plot for the second function
+plt.stem(x_values, z_values, linefmt='-', markerfmt='bo', basefmt='-', label='x_2(n)')
+
+# Highlight the point at n=12 with a different color
+plt.stem(12, my_function(12), linefmt='-', markerfmt='go', basefmt='-', label='n=12')
 
 plt.xlabel('n')
 plt.legend()
