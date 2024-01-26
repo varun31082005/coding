f6f26c7fd9324927a0943333da07ea68db542c66
varun <shakunavetisaisriramvarun@gmail.com>
2024-01-26 18:20:32 +0530

    second commit

--- 10.5.2-15/codes/x.py
+++ 10.5.2-15/codes/x.py
@@ -1,18 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Generate x values using linspace
-x_values = np.linspace(0, 14, 15)  # 29 points from -14 to 14
-
-y_values = np.where(x_values >= 0, 63 + 2 * x_values, 0)
-
-z_values = np.where(x_values >= 0, 3 + 7 * x_values, 0)
+data = np.loadtxt("data.txt", skiprows=0)
+x_values, y_values, z_values = data[:, 0], data[:, 1], data[:,2]
 
 
 # Plot the stem plots
 plt.stem(x_values, y_values, linefmt='-', markerfmt='ro', basefmt='-', label='x_1(n)')
 plt.stem(x_values, z_values, linefmt='-', markerfmt='bo', basefmt='-', label='x_2(n)')
 plt.stem(12, 87, linefmt='-', markerfmt='go', basefmt='-', label='n=12')
+print(x_values, y_values, z_values)
 
 # Set labels and legend
 plt.xlabel('n')
